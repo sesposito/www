b13c69ae8716eff28386d95a64b6152417fc828c
Simon Esposito <esposito.simon@gmail.com>
2015-06-12 16:56:13 +0100

    Latest version

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var lastFmNode = require('lastfm').LastFmNode;
+
+/* GET home page. */
+
+var lastfm = new lastFmNode({
+	api_key: 'dea8b9d2899c645d046e7e733abdc1a0',
+	secret: 'b8b9ce9ce3d60ee528eaec24fed01244'
+});
+
+var trackStream = lastfm.stream('s_mn');
+
+var musicInfo = {};
+
+trackStream.on('nowPlaying', function(track){
+	//console.log(track);
+	musicInfo['artist'] = track.artist['#text'];
+	musicInfo['track'] = track.name;
+	musicInfo['album'] = track.album['#text'];
+	musicInfo['nowPlaying'] = Boolean(track['@attr'].nowplaying);
+
+	track.image.forEach(function(url){
+		if(url.size === 'large'){
+			musicInfo['imageUrl'] = url['#text'];
+		}
+	});
+});
+
+trackStream.start();
+
+router.get('/', function(req, res) {
+	res.render('index', {musicInfo: musicInfo});
+});
+
+
+module.exports = router;
